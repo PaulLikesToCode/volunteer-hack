9b1d00ca7ac6321b3d10953167f982cf93f8c691
Eshe Pickett <eshe.pickett@gmail.com>
2014-09-23 17:07:05 -0700

    Added skills data

--- vol_max/get_volunteer_data.php
+++ vol_max/get_volunteer_data.php
@@ -5,4 +5,23 @@
 								'odca_site_1');
 	 $data = VolunteerMatchAPI::testing();
 	 print "$data";
+	 
+	 $skills_array = getSkillsFromFile("category_skills.txt");
+	 print_r($skills_array);
+	 
+	 function getSkillsFromFile ($file) {
+		$file_handle = fopen($file, "r") or die("Unable to open file $file!");
+		$skills = array();
+		// Output one line until end-of-file
+		while(!feof($file_handle)) {
+			$line = fgets($file_handle);
+			if (preg_match('/Category\s+:/',$line)) {
+				list($category,$skill) = explode(":",$line);	
+				array_push($skills,$skill);
+			} else {
+				array_push($line);
+			}
+		}
+		fclose($myfile);
+	 }
 //?>
