1734d1a6dfdb8e3c94bda4a8c9619a012499c883
paullikestocode <paul_schrage@yahoo.com>
2014-09-24 10:19:09 -0700

    With ajax call in javascript

--- js/vh.js
+++ js/vh.js
@@ -11,10 +11,35 @@ function searchVolunteerButton() {
 
 $('button.choice').on('click', searchVolunteerButton);
 
-$.ajax({
-	type: "GET",
-	url: "http://volunteer-hack-enpicket.15.126.231.221.xip.io/get_skills_data.php",
-	success: function(response) {
-		console.log(response);
-	}
-});
+// $.ajax({
+// 	type: "GET",
+// 	url: "http://volunteer-hack-enpicket.15.126.231.221.xip.io/get_skills_data.php",
+// 	success: function(response) {
+// 		console.log(response);
+// 	}
+// });
+
+function createProfile() {
+	var fname = $('#update-volunteer-fn').val();
+	var lname = $('#update-volunteer-ln').val();
+	var email = $('#update-volunteer-em').val();
+	var skills = $('#update-volunteer-skills').val().split(',');
+	$.ajax({
+		type: "POST",
+		url: "http://volunteer-hack-enpicket.15.126.231.221.xip.io/create_volunteer.php",
+		data: {
+			fname: fname,
+			lname: lname,
+			email: email,
+			skills: skills
+		}
+	})
+	.done(function() {
+		alert('Profile Created!');
+	})
+	.fail(function() {
+		alert("Sorry");
+	});
+}
+
+$('#update-volunteer-btn').on('click', createProfile);
